3ffb0502c727b65285ab322cee79cd7323623e2c
Alejandro Pinedo <alu96092@usj.es>
2026-02-07 19:00:50 +0100

    Pagina home cargada

---
+++ src/app/components/home/home.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { POKEMON_GAMES } from '../../models/pokemon-game.model';
+import { GameService } from '../../services/game-service';
+import { PokemonGame } from '../../models/pokemon-game.model';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+  games = POKEMON_GAMES;
+
+  public gameService = inject(GameService);
+
+  selectGame(game: PokemonGame) {
+    this.gameService.setActiveGame(game)
+  }
+}
--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,20 +1,11 @@
-import { Component, inject } from '@angular/core';
-import { POKEMON_GAMES } from '../../models/pokemon-game.model';
-import { PokemonGame } from '../../models/pokemon-game.model';
-import { GameService } from '../../services/game-service';
+import { Component } from '@angular/core';
+import { Home } from '../../components/home/home';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [Home],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
 export class HomePageComponent {
-  games = POKEMON_GAMES;
-
-  public gameService = inject(GameService);
-
-  selectGame(game: PokemonGame) {
-    this.gameService.setActiveGame(game)
-  }
 }
