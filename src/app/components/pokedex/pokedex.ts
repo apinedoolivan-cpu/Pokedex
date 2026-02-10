db77e5423189a0ccbba408c4d2ad1e6d795a68d1
Alejandro Pinedo <alu96092@usj.es>
2026-02-10 01:33:02 +0100

    Pagina correctamente implementada

---
+++ src/app/components/pokedex/pokedex.ts
@@ -0,0 +1,25 @@
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { GameService } from '../../services/game-service';
+import { PokedexStoreService } from '../../services/pokedex-store-service';
+import { PokemonDataService } from '../../services/pokemon-data-service';
+import { Pokemon } from '../../models/pokemon.model';
+
+@Component({
+  selector: 'app-pokedex',
+  imports: [RouterModule],
+  templateUrl: './pokedex.html',
+  styleUrl: './pokedex.scss',
+})
+export class PokedexComponent {
+  private gameService = inject(GameService);
+  private store = inject(PokedexStoreService);
+  private pokemonData = inject(PokemonDataService);
+
+  readonly activeGame = this.gameService.getActiveGame();
+  readonly pokemons = this.store.all;
+
+  pokemonSelected(pokemon: Pokemon){
+    this.pokemonData.selectedPokemon(pokemon);
+  }
+}
--- src/app/pages/pokedex/pokedex-page.ts
+++ src/app/pages/pokedex/pokedex-page.ts
@@ -1,25 +1,11 @@
-import { Component, inject } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { GameService } from '../../services/game-service';
-import { PokedexStoreService } from '../../services/pokedex-store-service';
-import { PokemonDataService } from '../../services/pokemon-data-service';
-import { Pokemon } from '../../models/pokemon.model';
+import { Component } from '@angular/core';
+import { PokedexComponent } from '../../components/pokedex/pokedex';
 
 @Component({
   selector: 'app-pokedex-page',
-  imports: [RouterModule],
+  imports: [PokedexComponent],
   templateUrl: './pokedex-page.html',
   styleUrl: './pokedex-page.scss',
 })
 export class PokedexPageComponent {
-  private gameService = inject(GameService);
-  private store = inject(PokedexStoreService);
-  private pokemonData = inject(PokemonDataService);
-
-  readonly activeGame = this.gameService.getActiveGame();
-  readonly pokemons = this.store.all;
-
-  pokemonSelected(pokemon: Pokemon){
-    this.pokemonData.selectedPokemon(pokemon);
-  }
 }
